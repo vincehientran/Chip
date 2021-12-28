dc5ccb73f50f2513a4a679a3b86e46f6d3da90f6
vincehientran <vincehientran@gmail.com>
2021-12-27 21:17:00 -0600

    change to only use gpiozero library

--- src/main.py
+++ src/main.py
@@ -1,35 +1,26 @@
-import RPi.GPIO as GPIO
-from gpiozero import Buzzer
+from gpiozero import Buzzer, LED, OutputDevice
 import time
 
+buzzer = Buzzer(4)
 
 def buzz(pitch, duration):
-    buzzer = Buzzer(4)
     period = 1.0 / pitch
     delay = period / 2
     cycles = int(duration * pitch)
     buzzer.beep(on_time=period, off_time=period, n=int(cycles/2))
 
-
 def main():
-    led = 12
-    relay = 11
+    led = LED(18)
+    relay = OutputDevice(17, active_high=True, initial_value=False)
 
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(led, GPIO.OUT)
-    GPIO.setup(relay, GPIO.OUT)
-
-
-    GPIO.output(led, GPIO.HIGH)
+    led.on()
 
     for _ in range(20):
-        GPIO.output(relay, GPIO.HIGH)
+        relay.on()
         buzz(float(260), 0.33)
         time.sleep(0.33)
-        GPIO.output(relay, GPIO.LOW)
+        relay.off()
         time.sleep(0.33)
-
-    GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
